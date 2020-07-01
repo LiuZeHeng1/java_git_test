ee236eb3b0525dcc350074f96ed2926dd29f45ee
LiuZeHeng1 <qq2590757239@outlook.com>
2020-07-02 07:31:34 +0800

    提交Chapter1

---
+++ Chapter1/src/dao/Demo.java
@@ -0,0 +1,14 @@
+package dao;
+
+public class Demo {
+
+
+    public void test(){
+        System.out.println("测试");
+    }
+
+    public void test3(){
+        System.out.println("test测试");
+    }
+
+}
